ff904ce2778ec232ac977c0fc4881459b9ad7c78
optimus252002 <135192970+optimus252002@users.noreply.github.com>
2023-06-01 11:56:44 +0700

    Add files via upload

--- resources/views/client/likesong.blade.php
+++ resources/views/client/likesong.blade.php
@@ -5,10 +5,10 @@
     }
 </style>
 @section('main-content')
-    <div class="header-content" style="background: #352667;height:240px;color: aliceblue">
-        <label for="" style=";font-weight: 600">Playlist</label>
-        <h2 for="" style="font-size: 70px;padding-top:20px;font-weight: 800">Danh sách phát của tôi #1</h2>
-        <div style="display: flex;padding-top:20px">
+    <div class="header-content" style="background: #352667;height:220px;color: aliceblue">
+        <label for="" style="font-weight: 600; margin: 16px 0 0 20px;">Playlist</label>
+        <h2 for="" style="font-size: 280%;margin:20px 0 16px 34px;font-weight: 800">Danh sách phát của tôi #1</h2>
+        <div style="display: flex;margin:34px 0 0 34px">
             <img src="{{$profile->avatar}}" alt=""
                 style="object-fit: cover;width: 30px;height: 30px; border-radius: 50%">
             <p>{{$profile->name}}</p>
@@ -18,7 +18,7 @@
 
     </div>
     <div class="footer-content" style="background: #15131c ;color: aliceblue">
-        <div style="display: flex;background: #1e1e1e">
+        <div style="display: flex;background: #1e1e1e; padding: 20px">
         <p style="padding-left:10px;">#</p>    
         <p style="padding-left:20px;">Title</p>    
         <p style="padding-left:30px;">Name</p>    
