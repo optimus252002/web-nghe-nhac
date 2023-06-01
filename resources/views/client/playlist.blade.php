41e7187550c3617ef9e9557ca847515da0ce86a3
optimus252002 <135192970+optimus252002@users.noreply.github.com>
2023-06-01 11:57:58 +0700

    Add files via upload

--- resources/views/client/playlist.blade.php
+++ resources/views/client/playlist.blade.php
@@ -5,10 +5,10 @@
             padding-left: 10px
         }
     </style>
-     <div class="header-content" style="background: #352667;height:240px;color: aliceblue">
-        <label for="" style=";font-weight: 600">Playlist</label>
-        <h2 for="" style="font-size: 70px;padding-top:20px;font-weight: 800">Danh sách phát của tôi #1</h2>
-        <div style="display: flex;padding-top:20px">
+     <div class="header-content" style="background: #352667;height:220px;color: aliceblue">
+        <label for="" style=" font-weight: 600; margin: 16px 0 0 20px; ">Playlist</label>
+        <h2 for="" style="font-size: 280%;margin:20px 0 16px 34px;font-weight: 800">Danh sách phát của tôi #1</h2>
+        <div style="display: flex;margin:34px 0 0 34px">
             <img src="{{$profile->avatar}}" alt=""
                 style="object-fit: cover;width: 30px;height: 30px; border-radius: 50%">
             <p>{{$profile->name}}</p>
@@ -18,9 +18,9 @@
 
     </div>
     <div class="footer-content" style="background: #212121;height:100vh;color: aliceblue">
-        <h4 style="font-weight: 800;font-size: 20px">Hãy cùng tìm nội dung cho danh sách phát của bạn</h4>
-        <input for="search" class="searchPlaylist" type="text" placeholder="Bạn muốn nghe gì?">
-        <div class="playlist-song">
+        <h4 style="font-weight: 800;font-size: 20px; margin: 0px 0 20px 26px; padding-top: 20px">Hãy cùng tìm nội dung cho danh sách phát của bạn</h4>
+        <input style="margin-left: 34px; height: 36px; width: 240px; padding: 16px; border-radius: 12px; " for="search" class="searchPlaylist" type="text"  placeholder="Bạn muốn nghe gì?">
+        <div style="margin-top: 24px; margin-left: 40px;" class="playlist-song">
 
         </div>
     </div>
